d7646668350fc74914669321f56294d03ce84017
joostvanrijsinge <jjrijsin@student.avans.nl>
2016-11-07 13:39:59 +0100

    Fix: Resources in res folder
    Moved all the resources into the res folder.

--- tmx_poc/main.cpp
+++ tmx_poc/main.cpp
@@ -5,7 +5,7 @@
 int main( void )
 {
     WorldParser* world_parser = new WorldParser();
-    Level* level_test = world_parser->generate_level( "live_test.tmx" );//test.tmx, test2.tmx or stress_test.tmx
+    Level* level_test = world_parser->generate_level( "res/test.tmx" );//test.tmx, test2.tmx or stress_test.tmx
     
     delete world_parser;
     return 0;
